7190d2fad4b2d66de22e732cc7bc3c940a2e217b
AndersonDaSilvaOSF <43174799+AndersonDaSilvaOSF@users.noreply.github.com>
2020-04-29 20:40:09 -0400

    first commit

---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+// import appointmentsRoutes from './appointments.routes';
+
+const routes = Router();
+
+// routes.use('/appointments', appointmentsRoutes);
+
+routes.get('/', (request, response) => response.json({ message: 'Hello Stack' }));
+
+export default routes;
