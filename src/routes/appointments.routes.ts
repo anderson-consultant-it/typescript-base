f5d5dce976abbf10f5970bced3ea8e06b85f05c4
AndersonDaSilvaOSF <43174799+AndersonDaSilvaOSF@users.noreply.github.com>
2020-05-10 15:40:44 -0400

    Added Model and Repository

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (req, res) => {
-  return res.json({});
+  const { provider, date } = req.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const appointment = appointmentsRepository.create(provider, parsedDate);
+  return res.json(appointment);
 });
 
 export default appointmentsRouter;
